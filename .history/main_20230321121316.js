23aabc08d210692c2c9e517c846cda187dffc952
miguel.pereira <miguel.pereira@sky.uk>
2023-03-21 12:20:26 +0000

    changes main path

--- .history/main_20230321121316.js
+++ .history/main_20230321121316.js
@@ -1,14 +1,14 @@
-import { setupCounter } from "./counter.js";
-import javascriptLogo from "./javascript.svg";
-import "./style.css";
-import viteLogo from "/vite.svg";
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-document.querySelector("#app").innerHTML = `
+document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href©="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
@@ -19,6 +19,6 @@ document.querySelector("#app").innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`;
+`
 
-setupCounter(document.querySelector("#counter"));
+setupCounter(document.querySelector('#counter'))
